35b4234b8fb69cc1c607f220f67cad8f7cd3c7f9
belaeat <omyhossain@gmail.com>
2023-07-07 00:19:40 +0600

    contact page added

--- README.md
+++ README.md
@@ -1,17 +1,8 @@
 # My Portfolio Website
 
-[🔗Live Demo🔗](https://sajib.vercel.app/)
+# [🔗Live Demo🔗](https://sajib.vercel.app/)
 
 ![Protfolio Website](https://i.ibb.co/JvWLMhB/portfolio.jpg)
 
 <br/>
 
-## Features
-
-**📖 Multi-Page Layout**
-
-**🎨 Styled with React-Bootstrap and CSS with easy to customize colors**
-
-**📱 Fully Responsive**
-
-<br />
--- src/components/Contact/Social.jsx
+++ src/components/Contact/Social.jsx
@@ -10,60 +10,44 @@ import './Social.css'
 
 const Social = () => {
   return (
-    <Container style={{padding: '30px'}}>
-    <Row>
-              <Col md={12} className="contact-social">
-                <div className='contact-text'>
-                <h1>FIND ME ON</h1>
-                <p>
-                Please don't hesitate to reach out to me and <span className="yellow">connect.</span>
-                </p>
-                </div>
-                <ul className="contact-social-links">
-                  <li className="contact-icons">
-                    <a
-                      href="https://github.com/19sajib"
-                      target="_blank"
-                      rel="noreferrer"
-                      className="icon-color  contact-social-icons"
-                    >
-                      <AiFillGithub />
-                    </a>
-                  </li>
-                  <li className="contact-icons">
-                    <a
-                      href="https://twitter.com/19sajib"
-                      target="_blank"
-                      rel="noreferrer"
-                      className="icon-color  contact-social-icons"
-                    >
-                      <AiOutlineTwitter />
-                    </a>
-                  </li>
-                  <li className="contact-icons">
-                    <a
-                      href="https://www.linkedin.com/in/19sajib/"
-                      target="_blank"
-                      rel="noreferrer"
-                      className="icon-color  contact-social-icons"
-                    >
-                      <FaLinkedinIn />
-                    </a>
-                  </li>
-                  <li className="contact-icons">
-                    <a
-                      href="https://www.instagram.com/19sajib"
-                      target="_blank"
-                      rel="noreferrer"
-                      className="icon-color contact-social-icons"
-                    >
-                      <AiFillInstagram />
-                    </a>
-                  </li>
-                </ul>
-              </Col>
-            </Row>
-          </Container>
+    <Container style={{ padding: '30px' }}>
+      <Row>
+        <Col md={12} className="contact-social">
+          <div className='contact-text'>
+            <h1>FIND ME ON</h1>
+            <p>
+              Please don't hesitate to reach out to me and <span className="yellow">connect.</span>
+            </p>
+          </div>
+          <ul className="contact-social-links">
+
+            {/* Github */}
+            <li className="contact-icons">
+              <a
+                href="https://github.com/belaeat/"
+                target="_blank"
+                rel="noreferrer"
+                className="icon-color  contact-social-icons"
+              >
+                <AiFillGithub />
+              </a>
+            </li>
+
+            {/* LinkedIn */}
+            <li className="contact-icons">
+              <a
+                href="https://www.linkedin.com/in/belaeat/"
+                target="_blank"
+                rel="noreferrer"
+                className="icon-color  contact-social-icons"
+              >
+                <FaLinkedinIn />
+              </a>
+            </li>
+          </ul>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
