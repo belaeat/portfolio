9656aa8ca8fc1385e27bde14c7aea217aba3e597
belaeat <omyhossain@gmail.com>
2023-07-21 18:17:53 +0600

    resume updated with portfolio

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -2,17 +2,15 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import { AiOutlineDownload } from "react-icons/ai";
-
 import Particle from '../components/Particle'
 import pdf from "../assets/belaeat.pdf"
-
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/esm/Page/TextLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
+// Resume updated with portfolio
 const resumeLink = `https://raw.githubusercontent.com/belaeat/portfolio/main/src/assets/belaeat.pdf`
-
 
 const Resume = () => {
   const [width, setWidth] = useState(1200);
